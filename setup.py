2d6008643f39358debde9dd89098d9a6d6d1e339
Matvei <matveiantipov2007@gmail.com>
2024-10-17 08:51:04 +0300

    Version 0.1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mathhunt",
-    version="0.1.0",
+    version="0.1.0.1",
     author="Matvei Antipov",
     author_email="matveiantipov2007@gmail.com",
     description="Toolkit for math calculations.",
