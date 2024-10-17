45df6f192cd720ff4736562e7558224276cfc721
Matvei <matveiantipov2007@gmail.com>
2024-10-17 09:14:28 +0300

    Bugfix 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mathhunt",
-    version="0.2.0",
+    version="0.2.1",
     author="Matvei Antipov",
     author_email="matveiantipov2007@gmail.com",
     description="Toolkit for math calculations.",
@@ -11,6 +11,9 @@ setup(
     url="https://github.com/Matvei-Antipov/mathhunt-0.1.0.git",
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        '': ['__data/bradis_table.json'],  # Указываем файл
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
